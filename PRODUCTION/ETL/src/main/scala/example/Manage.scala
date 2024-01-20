d8a5c3dd1597072b1f0c5f2692f8aa506f7c82a4
AshikJenly <jenlyashik@gmail.com>
2024-01-20 12:06:02 +0530

    updated eventhub etl

---
+++ PRODUCTION/ETL/src/main/scala/example/Manage.scala
@@ -0,0 +1,14 @@
+package example
+
+import org.apache.spark.sql.SparkSession
+
+object Manage
+{
+    def main(args:Array[String]) = {
+
+        private val spark = SparkSession.builder().getOrCreate() 
+        val event_hub_to_data_lake = new EventHubToDataLake(spark)
+        event_hub_to_data_lake.start
+        
+    }
+}
