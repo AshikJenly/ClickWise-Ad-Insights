6ef78d74680f26d47b9e457ba5b2f28afe6f070e
AshikJenly <jenlyashik@gmail.com>
2024-01-22 01:48:39 +0530

    Updated  manage code.

--- PRODUCTION/ETL/src/main/scala/example/Manage.scala
+++ PRODUCTION/ETL/src/main/scala/example/Manage.scala
@@ -7,9 +7,9 @@ object Manage
     def main(args:Array[String]) = {
 
         val spark = SparkSession.builder().getOrCreate() 
-        // val event_hub_to_data_lake = new EventHubToDataLake(spark)
+        val event_hub_to_data_lake = new EventHubToDataLake(spark)
         val data_lake_to_cosmos_db = new DataLakeToCosmosDB(spark)
-        // event_hub_to_data_lake.start
+        event_hub_to_data_lake.start
         data_lake_to_cosmos_db.start
         
     }
