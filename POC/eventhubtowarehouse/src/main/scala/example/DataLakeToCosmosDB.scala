b8c71b89e1f6b02194dc60f1ba6cf210cbc744c7
AshikJenly <jenlyashik@gmail.com>
2024-01-21 22:28:39 +0530

    updated write to mongo code

--- POC/eventhubtowarehouse/src/main/scala/example/DataLakeToCosmosDB.scala
+++ POC/eventhubtowarehouse/src/main/scala/example/DataLakeToCosmosDB.scala
@@ -7,9 +7,10 @@ import org.apache.spark.sql.streaming._
 
 object DataLakeToCosmos extends App {
   val spark = SparkSession.builder.appName("DataLakeToCosmos").getOrCreate()
+      spark.conf.set("spark.sql.streaming.stateStoreRetention", "1 minutes")
 
   val schema = StructType(Seq(
-    StructField("event_timestamp", TimestampType, nullable = false),
+    StructField("event_timestamp", StringType, nullable = false),
     StructField("user_id", StringType, nullable = false),
     StructField("session_id", StringType, nullable = false),
     StructField("page_url", StringType, nullable = false),
@@ -17,38 +18,40 @@ object DataLakeToCosmos extends App {
     StructField("browser", StringType, nullable = false),
     StructField("geo_location", StringType, nullable = false),
     StructField("event_type", StringType, nullable = false),
-    StructField("ad_clicked", BooleanType, nullable = false),
+    StructField("ad_clicked", StringType, nullable = false),
     StructField("ad_id", StringType, nullable = false),
-    StructField("duration_seconds", IntegerType, nullable = false),
-    StructField("Month", IntegerType, nullable = false),
-    StructField("year", IntegerType, nullable = false)
+    StructField("duration_seconds", StringType, nullable = false),
+    StructField("Month", StringType, nullable = false),
+    StructField("year", StringType, nullable = false)
   ))
 
 
-  var df  = spark.readStream.schema(schema).parquet("/warehouse/click")
+  var df  = spark.readStream
+                 .schema(schema)
+                 .parquet("/warehouse/click")
+                 .withColumn("event_timestamp", col("event_timestamp").cast(TimestampType))
+                 .withColumn("ad_clicked", col("ad_clicked").cast(BooleanType))
+                 
 
-      df  = df.filter(col("event_timestamp") > to_timestamp(lit("2023-12-30")))
-                  .groupBy(window(col("event_timestamp"), "100 day").alias("window"))
-                  .agg(
-                        count("user_id").alias("total_users_visited"),
-                        countDistinct("user_id").alias("total_unique_users_visited"),
-                        avg("duration_seconds").alias("avg_time_spend_in_website"),
-                        sum(when(col("ad_clicked"),1).otherwise(0)).alias("add_watched")
-                        )
+      df  = df
+            .withWatermark("event_timestamp","5 minutes")
+            .groupBy(window(col("event_timestamp"), "1 minutes").alias("window"))
+            .agg(
+                  count("user_id").alias("total_users_visited"),
+                  approx_count_distinct("user_id").alias("total_unique_users_visited"),
+                  avg("duration_seconds").alias("avg_time_spend_in_website"),
+                  sum(when(col("ad_clicked"),1).otherwise(0)).alias("add_watched")
+                  )
 
 
 
-// def writeToMongo(df: org.apache.spark.sql.Dataset[org.apache.spark.sql.Row]){df.show;df.write.option("uri","mongodb://localhost:27017/click.test").mode("overwrite").format("mongo").save}
-//    resultDF.writeStream.outputMode("complete").foreachBatch {(x:org.apache.spark.sql.Dataset[org.apache.spark.sql.Row], y:scala.Long) => writeToMongo(x) }.option("checkpointLocation", "/checkpoint/click").start
-
-   val query = resultDF.writeStream
-           .outputMode("overwrite")
-           .format("mongo")
-           .option("uri","mongodb://localhost:27017/click.test")
+def writeToMongo(df: org.apache.spark.sql.Dataset[org.apache.spark.sql.Row]){df.show;df.write.option("uri","mongodb://localhost:27017/click.test").mode("overwrite").format("mongo").save}
+   val query = df.writeStream
+           .outputMode("complete")
+           .foreachBatch {(x:org.apache.spark.sql.Dataset[org.apache.spark.sql.Row], y:scala.Long) => writeToMongo(x) }
            .option("checkpointLocation", "/checkpoint/click")
            .start.awaitTermination()
 
 //   val query = resultDF.writeStream
 //     .outputMode("complete").format("console").option("checkpointLocation", "/checkpoint/click").start()
-  query.awaitTermination()
 }
--- POC/eventhubtowarehouse/src/main/scala/example/EventHubToDataLakeETL.scala
+++ POC/eventhubtowarehouse/src/main/scala/example/EventHubToDataLakeETL.scala
@@ -15,22 +15,24 @@
 //             StructField("ad_clicked", BooleanType, nullable = false),
 //             StructField("ad_id", StringType, nullable = false),
 //             StructField("duration_seconds", IntegerType, nullable = false),
-//             StructField("Month", IntegerType, nullable = false),
-//             StructField("year", IntegerType, nullable = false)
+//             // StructField("Month", IntegerType, nullable = false),
+//             // StructField("year", IntegerType, nullable = false)
 //             ))
       
 //             var df = spark.readStream.schema(schema).option("header",true).csv("/data/click")
 //             df = df.withColumn("Month",month(col("event_timestamp")))
 //             df = df.withColumn("year",year(col("event_timestamp")))
 
-//             val res = df.writeStream
-//                         .outputMode("append")
-//                         .partitionBy("year","Month")
-//                         .format("parquet")
-//                         .option("checkpointLocation","/checkpoint/click")
-//                         .option("path","/warehouse/click")
+//             // val res = df.writeStream
+//             //             .outputMode("append")
+//             //             .partitionBy("year","Month")
+//             //             .format("parquet")
+//             //             .option("checkpointLocation","/checkpoint/click")
+//             //             .option("path","/warehouse/click")
+//             //             .start()
+//             //             .awaitTermination
+//  val res = df.writeStream.outputMode("append").partitionBy("year","Month").format("parquet").option("checkpointLocation","/checkpointfe/click").option("path","/warehouse/click").start
 //                         .start()
 //                         .awaitTermination
-
 // }
 
