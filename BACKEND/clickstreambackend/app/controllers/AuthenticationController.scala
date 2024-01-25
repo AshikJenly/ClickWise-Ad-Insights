37043dc7697c0bf3fa64ad44c430953ca0859633
AshikJenly <jenlyashik@gmail.com>
2024-01-25 19:27:53 +0530

    Added play starter

---
+++ BACKEND/clickstreambackend/app/controllers/AuthenticationController.scala
@@ -0,0 +1,10 @@
+package controllers
+
+import javax.inject._
+import play.api._
+import play.api.mvc._
+
+class AuthenticationController @Inject()(cc: ControllerComponents) extends AbstractController(cc){
+
+    
+}
