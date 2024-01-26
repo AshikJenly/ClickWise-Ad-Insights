dc7df7ad0ced1b7cd36f6186329315c61bc93d15
AshikJenly <jenlyashik@gmail.com>
2024-01-26 23:24:38 +0530

    updated userDAO

---
+++ BACKEND/clickstreambackend/app/dao/UsersDAO.scala
@@ -0,0 +1,32 @@
+package dao
+
+import models._
+import javax.inject.Inject
+import play.api.db.slick.DatabaseConfigProvider
+import slick.jdbc.MySQLProfile
+import slick.jdbc.MySQLProfile.api._
+
+import scala.concurrent.{ExecutionContext, Future}
+
+
+case class UsersDAO @Inject()(protected val dbConfigProvider: DatabaseConfigProvider)(implicit ec: ExecutionContext) {
+
+    private val dbConfig = dbConfigProvider.get[MySQLProfile]
+    private class UsersTable(tag:Tag) extends Table[User](tag,"users"){
+
+        def id = column[Long]("id", O.PrimaryKey, O.AutoInc)
+        def name = column[String]("name")
+        def email = column[String]("email")
+        def password = column[String]("password")
+
+        def * = (id.?, name,email,password) <> (User.tupled, User.unapply)
+  
+    }
+    private val Users = TableQuery[UsersTable]
+    
+
+
+    def getAllUsers: Future[Seq[User]] = {
+            dbConfig.db.run(Users.result)
+    }
+}
--- BACKEND/clickstreambackend/app/models/User.scala
+++ BACKEND/clickstreambackend/app/models/User.scala
@@ -1,5 +1,5 @@
 package models
 
-case class User(name:String,email:String,password:String){
+case class User(id: Option[Long],name:String,email:String,password:String){
     override def toString = s"Name : $name\nemail : $email"
 }
