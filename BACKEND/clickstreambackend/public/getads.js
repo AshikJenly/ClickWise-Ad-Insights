24ce640b024f3c43f0a3872679bf1a7b87206210
AshikJenly <jenlyashik@gmail.com>
2024-01-30 01:10:50 +0530

    updated random add show

--- BACKEND/clickstreambackend/public/getads.js
+++ BACKEND/clickstreambackend/public/getads.js
@@ -1,6 +1,6 @@
 
 
-let adData = []; 
+let AD_DATA = []; 
 
 
 async function fetchData() {
@@ -9,8 +9,8 @@ async function fetchData() {
             method: 'GET'
         });
         const fetchedData = await response.json();
-        adData = fetchedData;
-        console.log(adData);
+        AD_DATA = fetchedData;
+        console.log(AD_DATA);
     } catch (error) {
         console.error('Error fetching data:', error);
     }
@@ -18,4 +18,22 @@ async function fetchData() {
 
 async function initialize() {
     await fetchData();
+}
+function getRandomAd(array=AD_DATA) {
+    const randomIndex = Math.floor(Math.random() * array.length);
+    return array[randomIndex];
+}
+// console.log(getRandomAd())
+
+function updateDisplay(data) {
+    // Get the elements you want to update
+    const adCard = document.getElementById('adCard');
+    const imageElement = adCard.querySelector('.card-img-top');
+    const titleElement = adCard.querySelector('.card-title');
+    const textElement = adCard.querySelector('.card-text');
+
+    // Update the HTML content with the values from the data
+    imageElement.src = data.adimage;
+    titleElement.innerText = data.adcontent;
+    textElement.innerText = "This is the ad content. You can customize it based on your data.";
 }
