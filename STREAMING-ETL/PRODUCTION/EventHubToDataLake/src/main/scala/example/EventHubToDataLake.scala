465fe217bc4bd951c508cb905864eff75af26347
AshikJenly <jenlyashik@gmail.com>
2024-01-31 21:03:00 +0530

    updated event hub Data

--- STREAMING-ETL/PRODUCTION/EventHubToDataLake/src/main/scala/example/EventHubToDataLake.scala
+++ STREAMING-ETL/PRODUCTION/EventHubToDataLake/src/main/scala/example/EventHubToDataLake.scala
@@ -10,18 +10,18 @@ case class EventHubToDataLake(private val spark:SparkSession)
 {
      
      private val schema = StructType(Seq(
-            StructField("event_timestamp", TimestampType, nullable = false),
-            StructField("user_id", StringType, nullable = false),
-            StructField("session_id", StringType, nullable = false),
-            StructField("page_url", StringType, nullable = false),
-            StructField("device_type", StringType, nullable = false),
+            StructField("event_timestamp", StringType, nullable = false),
+            StructField("userId", StringType, nullable = false),
+            StructField("sessionId", StringType, nullable = false),
+            StructField("pageUrl", StringType, nullable = false),
+            StructField("deviceType", StringType, nullable = false),
             StructField("browser", StringType, nullable = false),
-            StructField("geo_location", StringType, nullable = false),
-            StructField("event_type", StringType, nullable = false),
-            StructField("ad_clicked", BooleanType, nullable = false),
-            StructField("ad_id", StringType, nullable = false),
-            StructField("duration_seconds", IntegerType, nullable = false)
-            ))
+            StructField("geoLocation", StringType, nullable = false),
+            StructField("eventType", StringType, nullable = false),
+            StructField("adClicked", BooleanType, nullable = false),
+            StructField("adId", StringType, nullable = false),
+            StructField("durationSeconds", IntegerType, nullable = false)
+))
       
       private val CONNECTION_STRING = "Endpoint=sb://forspark.servicebus.windows.net/;SharedAccessKeyName=produce;SharedAccessKey=7hiTBO6qCXCgXVDEmHr40y+RFUMHAJGoA+AEhKvCiII=;EntityPath=clickstream" 
       
@@ -32,20 +32,22 @@ case class EventHubToDataLake(private val spark:SparkSession)
       def start = {
       
                   println(incomingStream.printSchema)
-                  var df= incomingStream.selectExpr("cast(body as string) as json").select(from_json(col("json"),schema).alias("sdata"))
-
-                  df = df.select("sdata.event_timestamp",
-                              "sdata.user_id",
-                              "sdata.session_id",
-                              "sdata.page_url",
-                              "sdata.device_type",
-                              "sdata.browser",
-                              "sdata.geo_location",
-                              "sdata.event_type",
-                              "sdata.ad_clicked",
-                              "sdata.ad_id",
-                              "sdata.duration_seconds"
-                              )
+                  
+                  val df = incomingStream.select(col("body").cast(StringType).alias("json"))
+                                         .select(from_json(col("json"), schema).alias("sdata"))
+                                         .select(
+                                          "sdata.event_timestamp",
+                                          "sdata.userId",
+                                          "sdata.sessionId",
+                                          "sdata.pageUrl",
+                                          "sdata.deviceType",
+                                          "sdata.browser",
+                                          "sdata.geoLocation",
+                                          "sdata.eventType",
+                                          "sdata.adClicked",
+                                          "sdata.adId",
+                                          "sdata.durationSeconds"
+                                    )
 
                   df = df.withColumn("Month",month(col("event_timestamp")))
                   df = df.withColumn("year",year(col("event_timestamp")))
