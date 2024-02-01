dd7b92a4e3630b1cd7c135c6d30c071f604540e8
AshikJenly <jenlyashik@gmail.com>
2024-02-01 11:24:18 +0530

    Updated DataLake to Event hub code

--- STREAMING-ETL/PRODUCTION/DataLakeToCosmos/src/main/scala/example/DataLakeToCosmosDB.scala
+++ STREAMING-ETL/PRODUCTION/DataLakeToCosmos/src/main/scala/example/DataLakeToCosmosDB.scala
@@ -9,41 +9,38 @@ import org.apache.spark.sql.streaming.Trigger
 case class DataLakeToCosmosDB (private val spark:SparkSession) {
     //   spark.conf.set("spark.sql.streaming.stateStoreRetention", "10 minutes")
 
-    private val schema = StructType(Seq(
-                    StructField("event_timestamp", StringType, nullable = true),
-                    StructField("user_id", StringType, nullable = true),
-                    StructField("session_id", StringType, nullable = true),
-                    StructField("page_url", StringType, nullable = true),
-                    StructField("device_type", StringType, nullable = true),
-                    StructField("browser", StringType, nullable = true),
-                    StructField("geo_location", StringType, nullable = true),
-                    StructField("event_type", StringType, nullable = true),
-                    StructField("ad_clicked", BooleanType, nullable = true),
-                    StructField("ad_id", StringType, nullable = true),
-                    StructField("duration_seconds", IntegerType, nullable = true),
-                    StructField("Month", IntegerType, nullable = true),
-                    StructField("year", IntegerType, nullable = true)
-                ))
+      private val schema = StructType(Seq(
+            StructField("event_timestamp", TimestampType, nullable = false),
+            StructField("userId", StringType, nullable = false),
+            StructField("sessionId", StringType, nullable = false),
+            StructField("pageUrl", StringType, nullable = false),
+            StructField("deviceType", StringType, nullable = false),
+            StructField("browser", StringType, nullable = false),
+            StructField("geoLocation", StringType, nullable = false),
+            StructField("eventType", StringType, nullable = false),
+            StructField("adClicked", BooleanType, nullable = false),
+            StructField("adId", StringType, nullable = false),
+            StructField("durationSeconds", IntegerType, nullable = false)
+))
+      
 
     private val CONNECTION_URI = "mongodb://forspark:J6cznn1Wd7ukY4lNNxqhhyJxVRm4aBIH8WrAqrHspTjbKTW5KvrIEqHdL9SvsyLmZziicHF4M3v8ACDbna0CdA==@forspark.mongo.cosmos.azure.com:10255/click.test?ssl=true&retrywrites=false&replicaSet=globaldb&maxIdleTimeMS=120000&appName=@forspark@"
-    var df  = spark.readStream
+    var data  = spark.readStream
                             .schema(schema)
-                            .parquet("/mnt/streamingdata/clickstream/warehouse/click")
-                            .withColumn("event_timestamp", col("event_timestamp").cast(TimestampType))
+                            .parquet("/mnt/streamingdatafinal/clickstream/warehouse/click")
+                            // .withColumn("event_timestamp", col("event_timestamp").cast(TimestampType))
                             // .withColumn("ad_clicked", col("ad_clicked").cast(BooleanType))
         
     def start = {
         println("Printing Schema")
-        println(df.printSchema)
-        df  = df
-            .withWatermark("event_timestamp","1 day")
-            .groupBy(window(col("event_timestamp"), "1 hour").alias("window"))
-            .agg(
-                  count("user_id").alias("total_users_visited"),
-                  approx_count_distinct("user_id").alias("total_unique_users_visited"),
-                  avg("duration_seconds").alias("avg_time_spend_in_website"),
-                  sum(when(col("ad_clicked"),1).otherwise(0)).alias("add_watched")
-                  )
+        println(data.printSchema)
+       var df  = data.withWatermark("event_timestamp","1 hour")
+                    .groupBy(window(col("event_timestamp"), "5 seconds").alias("window"))
+                        .agg(
+                            count("userId").alias("total_users_visited"),
+                            approx_count_distinct("userId").alias("total_unique_users_visited"),
+                            avg("durationSeconds").alias("avg_time_spend_in_website"),
+                            sum(when(col("adClicked"),1).otherwise(0)).alias("add_watched"))
 
 
 
@@ -52,7 +49,7 @@ case class DataLakeToCosmosDB (private val spark:SparkSession) {
         val query = df.writeStream
                 .outputMode("update")
                 .foreachBatch {(x:org.apache.spark.sql.Dataset[org.apache.spark.sql.Row], y:scala.Long) => writeToMongo(x) }
-                .option("checkpointLocation", "/mnt/streamingdata/clickstreamcheckpoint/asls_to_cosmos/check")
+                .option("checkpointLocation", "/mnt/streamingdatafinal/clickstreamcheckpoint/asls_to_cosmos/finalcheck")
                 // .trigger(Trigger.ProcessingTime("4 seconds"))
                 .start.awaitTermination()
     }
