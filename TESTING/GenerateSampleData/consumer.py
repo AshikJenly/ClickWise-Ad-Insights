35192ce5f1665a6e272c3a953ef6798a8b9ca289
AshikJenly <jenlyashik@gmail.com>
2024-02-01 11:31:39 +0530

    updated testing code

---
+++ TESTING/GenerateSampleData/consumer.py
@@ -0,0 +1,33 @@
+from azure.eventhub import EventHubConsumerClient
+
+# Event Hubs connection string
+CONNECTION_STRING = "Endpoint=sb://forspark.servicebus.windows.net/;SharedAccessKeyName=produce;SharedAccessKey=7hiTBO6qCXCgXVDEmHr40y+RFUMHAJGoA+AEhKvCiII=;EntityPath=clickstream"  ##fill in with the connection string from EventHub
+
+# Event Hubs consumer group and partition
+CONSUMER_GROUP = "$Default"
+PARTITION_ID = "0"
+
+# Define callback function to process received events
+def on_event(partition_context, event):
+    print("Received event from partition: {}".format(partition_context.partition_id))
+    print("Data: {}".format(event.body_as_str()))
+    print("Properties: {}".format(event.properties))
+    print("SystemProperties: {}".format(event.system_properties))
+    print("--------")
+
+# Create Event Hub consumer client
+consumer_client = EventHubConsumerClient.from_connection_string(
+    conn_str=CONNECTION_STRING,
+    consumer_group=CONSUMER_GROUP
+)
+
+try:
+    # Start consuming events
+    with consumer_client:
+        consumer_client.receive(
+            on_event=on_event,
+            starting_position="-1",  # Start reading from the latest available event
+            partition_id=PARTITION_ID
+        )
+except KeyboardInterrupt:
+    print("Receiving has stopped.")
