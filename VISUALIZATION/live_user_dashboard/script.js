e9395ba63d926d8c1a7d32cc350d5887131ab057
AshikJenly <jenlyashik@gmail.com>
2024-02-03 16:06:32 +0530

    bug: fixed chart animations

--- VISUALIZATION/live_user_dashboard/script.js
+++ VISUALIZATION/live_user_dashboard/script.js
@@ -1,16 +1,18 @@
 // Data from your provided JSON
-const url = "http://20.193.151.118:9096/api/datas/mongo/agg";
+const url = "http://localhost:9096/api/datas/mongo/agg";
 const liveUsersCount = document.querySelector(".live-users__heading");
 const main = document.querySelector(".main");
 const loader = document.querySelector(".loadingspinner");
 const ctx = document.getElementById("myChart");
 const ctx1 = document.getElementById("donetChart");
-
+var chart1;
+var chart2;
 const hideLoader = () => {
   loader.classList.add("hide");
   main.classList.remove("hide");
 };
 const updateLiveUsers = (data) => {
+  
   const liveUser = data[data.length - 1].total_users_visited;
   //   if (liveUser < 10) {
   //     liveUsersCount.innerHTML = `10+`;
@@ -20,21 +22,27 @@ const updateLiveUsers = (data) => {
 };
 
 const getData = async () => {
+
   const res = await fetch(url);
   const data = await res.json();
   if (data) {
     hideLoader();
-    console.log("hello");
+    console.log('hello');
   }
 
   return updateChart(data);
 };
 
 const updateChart = (data) => {
+if(chart1){
+  chart1.destroy();
+  chart2.destroy();
+}
+  
   updateLiveUsers(data);
   const timestamp = data.map((item) => new Date(item._id.start.$date));
   const hours = timestamp.map((item) => item.getUTCHours());
-  new Chart(ctx1, {
+   chart1=new Chart(ctx1, {
     type: "doughnut",
     data: {
       labels: ["live users", "unique users"],
@@ -50,6 +58,11 @@ const updateChart = (data) => {
       ],
     },
     options: {
+      
+      animation: {
+        duration: 2000, // Animation duration in milliseconds
+        easing: 'easeInOutQuart', // Easing function for the animation (optional)
+    },
       responsive: true,
       plugins: {
         legend: {
@@ -63,7 +76,7 @@ const updateChart = (data) => {
     },
   });
 
-  new Chart(ctx, {
+ chart2 = new Chart(ctx, {
     type: "bar",
     data: {
       labels: hours,
@@ -77,6 +90,10 @@ const updateChart = (data) => {
       ],
     },
     options: {
+      animation: {
+        duration: 2000, // Animation duration in milliseconds
+        easing: 'easeInOutQuart', // Easing function for the animation (optional)
+    },
       responsive: true,
       plugins: {
         legend: {
@@ -91,8 +108,11 @@ const updateChart = (data) => {
     },
   });
 };
+
+
+
 setTimeout(() => {
   getData();
 }, 1000);
 
-setInterval(getData, 2000);
+setInterval(getData, 6000);
